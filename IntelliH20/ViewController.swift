20d6c93c3183957fcecbb697b94e8b397dba12c5
legozebra <legozebra@gmail.com>
2015-04-05 18:32:21 -0700

    Updates from LegoZebraMBP

--- IntelliH20/ViewController.swift
+++ IntelliH20/ViewController.swift
@@ -6,20 +6,76 @@
 //  Copyright (c) 2015 IntelliNinja Productions. All rights reserved.
 //
 
+
+
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, NRFManagerDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    
+    var nrfManager:NRFManager!
+    
+    override func viewDidLoad()
+    {
         // Do any additional setup after loading the view, typically from a nib.
+        
+        super.viewDidLoad()
+        nrfManager = NRFManager(delegate:self)
+        println("Created nrfManager")
     }
-
+    
+    func sendData()
+    {
+        let result = self.nrfManager.writeString("Hello, world!")
+    }
+    
+    // NRFManagerDelegate methods
+    
+    func nrfDidConnect(nrfManager:NRFManager)
+    {
+        println("Connected")
+        self.sendData()
+    }
+    
+    func nrfDidDisconnect(nrfManager:NRFManager)
+    {
+        println("Disconnected")
+    }
+    
+    func nrfReceivedData(nrfManager:NRFManager, data: NSData, string: String) {
+        println("Received data - String: \(string) - Data: \(data)")
+    }
+  
+    @IBOutlet weak var inputdata: UITextField!
+    @IBOutlet weak var answer: UILabel!
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func Button(sender: AnyObject) {
+        var answer1 = inputdata.text.toInt()
+  
+        println("in the loop")
+
+        if answer1 > -1 && answer1 < 40 {
+            answer.text = "Safe to drink!"
+            answer.textColor = UIColor.greenColor ()
+        } else if answer1 > 40 && answer1 < 70 {
+            answer.text = "Warning! Don't risk drinking."
+            answer.textColor = UIColor.yellowColor ()
+        } else if answer1 > 70 {
+            answer.text = "Do not drink!"
+            answer.textColor = UIColor.redColor ()
+        } else {
+            answer.text = "String or negative value entered"
+            answer.textColor = UIColor.redColor ()
+            
+        }
+    }
 
 }
 
